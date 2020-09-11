80856c8ba4650bc126cdfc921278898be371b26b
JosemarBrito <estudos.josemarbrito@gmail.com>
2020-09-10 21:39:20 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,20 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f'Ola Mundo{id(self)}'
+
+
+if __name__ == '__main__':
+    josemar = Pessoa(nome= 'Josemar')
+    guilherme = Pessoa(josemar, nome='Guilherme')
+    print(Pessoa.cumprimentar(guilherme))
+    print(id(guilherme))
+    print(guilherme.nome)
+    print(guilherme.idade)
+    for filho in guilherme.filhos:
+        print(filho.nome)
+
